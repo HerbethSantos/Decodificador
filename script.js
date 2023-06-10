c4311f0c6741d476e23006a9e7a09004e672ffda
Herbeth Santos <herbeth.conekt@gmail.com>
2023-06-10 19:28:39 -0300

    Chamando a função encriptar ao clicar no botão Criptografar.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const textArea = document.querySelector("text-area");
-const mensagem = document.querySelector("mensagem");
+const textArea = document.querySelector(".text-area");
+const mensagem = document.querySelector(".mensagem");
 
 // As "chaves" de criptografia que utilizaremos são:
 // A letra "e" é convertida para "enter"
@@ -8,15 +8,21 @@ const mensagem = document.querySelector("mensagem");
 // A letra "o" é convertida para "ober"
 // A letra "u" é convertida para "ufat"
 
-function encriptar () {
+function btnEncriptar () {
+    const textoEncriptado = encriptar(textArea.value);
+    mensagem.value = textoEncriptado
+    textArea.value = "";
+}
 
-    let matrizCodigo = [["e" , "enter"] , ["i" , "imes"] , ["a" , "ai"] , ["o" , "obter"] , ["u" , "ufat"]];
+function encriptar (stringEncriptada) {
+    
+    let matrizCodigo = [["e" , "enter"] , ["i" , "imes"] , ["a" , "ai"] , ["o" , "ober"] , ["u" , "ufat"]];
     stringEncriptada = stringEncriptada.toLowerCase();
 
     for(let i = 0; i < matrizCodigo.length; i++) {
         if(stringEncriptada.includes(matrizCodigo[i][0])) {
-            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
-        }     
+           stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
     }
 
     return stringEncriptada;
