156bc5e5f7bd67b5277363b5b1686cbdb5c66c12
Herbeth Santos <herbeth.conekt@gmail.com>
2023-06-10 19:40:46 -0300

    Descriptografrando.

--- script.js
+++ script.js
@@ -27,3 +27,24 @@ function encriptar (stringEncriptada) {
 
     return stringEncriptada;
 }
+
+function btnDesencriptar () {
+    const textoDesencriptado = desencriptar(textArea.value);
+    mensagem.value = textoDesencriptado
+    textArea.value = "";
+}
+
+
+function desencriptar (stringDesencriptada) {
+    
+    let matrizCodigo = [["e" , "enter"] , ["i" , "imes"] , ["a" , "ai"] , ["o" , "ober"] , ["u" , "ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++) {
+        if(stringDesencriptada.includes(matrizCodigo[i][1])) {
+           stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+        }
+    }
+
+    return stringDesencriptada;
+}
